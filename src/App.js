a9fc4840de6dd04faa6a65f8dd063e6a579b6b2e
miathayde <michelle.estevinho@hotmail.com>
2020-10-11 22:04:34 -0300

    alterações iniciais

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StylesProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+import GlobalStyle from './commons/styles/global-style'
+import Main from './containers/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <StylesProvider injectFirst>
+        <CssBaseline />
+        <GlobalStyle />
+        <Main />
+      </StylesProvider>
+    </div>
+  );
+}
+
+export default App;
